16052f222928a2c4c3adf6cdf9245057e6e67836
bbekjava <bibek.gupta@okstate.edu>
2018-11-28 19:51:23 -0600

    new

--- app/src/main/java/android/database/sqlite/SQLiteOpenHelper/DBHelper.java
+++ app/src/main/java/android/database/sqlite/SQLiteOpenHelper/DBHelper.java
@@ -1,26 +1,41 @@
 package android.database.sqlite.SQLiteOpenHelper;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.support.annotation.Nullable;
 
 public class DBHelper extends SQLiteOpenHelper {
+
     private static final String DB_NAME="TASKS";
     private static final String DB_COLUMN="TASKNAME";
     private static final int DB_VER=1;
 
     public DBHelper(Context context) {
-        super(context,DB_NAME, null, 1);
+        super(context, DB_NAME, null, DB_VER);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-       db.execSQL ("CREATE TABLE TASKS (ID INTEGER PRIMARY KEY AUTOINCREMENT, TASKNAME NOT NULL);");
+        String q = String.format("CREATE TABLE %s " +
+                "(ID INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                "%s TEXT NOT NULL);", DB_NAME, DB_COLUMN);
+        db.execSQL(q);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        db.execSQL("DELETE TABLE IF EXISTS TASK");
+        String q = String.format("DELETE TABLE IF EXISTS %s;", DB_NAME);
+        db.execSQL(q);
         onCreate(db);
+    }
+
+    public void insertTask(String task){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(DB_COLUMN,task);
+        db.insertWithOnConflict(DB_NAME, null, values,5);
+        db.close();
     }
 }
